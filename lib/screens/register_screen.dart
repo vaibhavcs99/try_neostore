e19bec0a7680ac364bd39b7c950c0b7ce75a6dff
vaibhavcs99 <vcsonawane99@gmail.com>
2021-01-07 16:46:57 +0530

    register screen implemented

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -6,29 +6,84 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  var usernameController = TextEditingController();
+  var maleCheckBox = false;
+  var femaleCheckBox = false;
+  var firstNameController = TextEditingController();
+  var lastNameController = TextEditingController();
+  var emailController = TextEditingController();
   var passwordController = TextEditingController();
+  var confirmPasswordController = TextEditingController();
+  var phoneNumberController = TextEditingController();
+  var gender;
+  @override
+  void dispose() {
+    firstNameController.dispose();
+    lastNameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    phoneNumberController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: ListView(
             children: [
-              TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(labelText: 'Username')),
-              TextField(
-                  controller: passwordController,
-                  decoration: InputDecoration(labelText: 'Password')),
-              TextField(
-                  controller: passwordController,
-                  decoration: InputDecoration(labelText: 'Password')),
-              RaisedButton(onPressed:()=> print('Pressed'), child: Text('Login'))
+              buildTextField('First Name',firstNameController),
+              buildTextField('Last Name',lastNameController),
+              buildTextField('Email',emailController),
+              buildTextField('Password',passwordController),
+              buildTextField('Confirm Password',confirmPasswordController),
+              buildGender(),
+              buildTextField('Phone Number',phoneNumberController),
+              RaisedButton(
+                  onPressed: () => print('Pressed'), child: Text('Login'))
             ],
           ),
         ));
+  }
+
+  Row buildGender() {
+    return Row(
+      children: [
+        Expanded(flex: 3, child: Text('Gender')),
+        Expanded(
+          flex: 1,
+          child: Checkbox(
+            value: maleCheckBox,
+            onChanged: (value) {
+              gender = 'Male';
+              setState(() {
+                maleCheckBox = value;
+              });
+            },
+          ),
+        ),
+        Expanded(flex: 1, child: Text('Male')),
+        Expanded(
+          flex: 1,
+          child: Checkbox(
+            value: maleCheckBox,
+            onChanged: (value) {
+              gender = 'Male';
+              setState(() {
+                maleCheckBox = value;
+              });
+            },
+          ),
+        ),
+        Expanded(flex: 1, child: Text('Female')),
+      ],
+    );
+  }
+
+  TextField buildTextField(String title, TextEditingController _controller) {
+    return TextField(
+        controller: _controller,
+        decoration: InputDecoration(labelText: '$title'));
   }
 }
